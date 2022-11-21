412dd9dbb60d3395f0f176d10f92f4d4af3620b7
IndiraElid101 <118759206+IndiraElid101@users.noreply.github.com>
2022-11-21 18:40:18 -0500

    Add files via upload

---
+++ js/prueba.js
@@ -0,0 +1,9 @@
+alert("Bienvenido");
+alert("LUIS");
+alert("DAVID");
+alert("♥ TE AMO ♥");
+alert("♥ ERES EL MEJOR ♥");
+alert("♥ TE ADORO ♥");
+alert("♥ ME ENCANTAS ♥");
+alert("♥ ME FASCINAS ♥");
+alert("♥ ME TIENES LOQUITA DE AMOR POR TI ♥");
